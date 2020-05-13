96b77119ad4db517b9b75b6ec583e9eaac895941
Michael <michael@pixt.us>
2020-05-13 14:50:57 +0700

    Updates

---
+++ precache-manifest.2d478f9a81201a97af04e1da84891619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751b7d889bd1ffb4a381721b1699e7b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d447df4d00eab4b8f8fa",
+    "url": "/static/css/main.525b898b.chunk.css"
+  },
+  {
+    "revision": "8976054a85b0b6cb0265",
+    "url": "/static/js/2.557bedf9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.557bedf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d447df4d00eab4b8f8fa",
+    "url": "/static/js/main.389ba25a.chunk.js"
+  },
+  {
+    "revision": "ede81b23cd98fe4f9780",
+    "url": "/static/js/runtime-main.8ec15f7a.js"
+  },
+  {
+    "revision": "aadc7ac7041b61af3ebb6ff2f62be364",
+    "url": "/static/media/arrow-right.aadc7ac7.svg"
+  }
+]);
